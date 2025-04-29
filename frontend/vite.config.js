01c6d3059d3217005cf3d8681a27715526f12d2c
jh9098 <90673222+jh9098@users.noreply.github.com>
2025-04-29 22:37:49 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,47 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
-import viteCompression from 'vite-plugin-compression';
-import viteImagemin from 'vite-plugin-imagemin';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-      modernPolyfills: true,
-    }),
-    viteCompression({
-      algorithm: 'gzip',
-      ext: '.gz',
-      threshold: 10240,
-      deleteOriginFile: false,
-    }),
-    viteCompression({
-      algorithm: 'brotliCompress',
-      ext: '.br',
-      threshold: 10240,
-      deleteOriginFile: false,
-    }),
-    viteImagemin({
-      gifsicle: { optimizationLevel: 7 },
-      optipng: { optimizationLevel: 7 },
-      mozjpeg: { quality: 75 },
-      pngquant: { quality: [0.65, 0.9], speed: 4 },
-      svgo: {
-        plugins: [
-          { name: 'removeViewBox', active: false },
-          { name: 'removeEmptyAttrs', active: false },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   envPrefix: 'VITE_',
   build: {
     assetsInlineLimit: 4096,
     outDir: 'dist',
     sourcemap: false,
-    cssCodeSplit: true,
     rollupOptions: {
       output: {
         manualChunks(id) {
@@ -52,6 +19,5 @@ export default defineConfig({
       }
     },
     minify: 'esbuild',
-    chunkSizeWarningLimit: 1000,
   },
 });
